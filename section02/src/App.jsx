d594a17674bc541a9f6a81b9eb5a6270cf03e41e
nz663633 <hyunjisim3@gmail.com>
2026-02-07 21:49:35 +0900

    docs: study React component basics

--- section02/src/App.jsx
+++ section02/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-function App() {
-
+function App() { // 부모 컴포넌트(root 컴포넌트)
   return (
     <>
+      <Header />
+      <Main />
+      <Footer />
       <h1>안녕 리액트!</h1>
     </>
-  )
-}
+  );
+};
 
 export default App
