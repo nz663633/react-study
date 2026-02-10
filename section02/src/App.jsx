775d52947136664d6a051283fc633edbeea2c904
nz663633 <hyunjisim3@gmail.com>
2026-02-10 16:53:14 +0900

    docs: study React state management with useState

--- section02/src/App.jsx
+++ section02/src/App.jsx
@@ -1,25 +1,26 @@
 import './App.css'
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Button from './components/Button';
+import { useState } from 'react';
 
-function App() { // 부모 컴포넌트(root 컴포넌트)
+function App() {
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
 
-  const buttonProps = { // 객체로 만들어 spread 사용
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  }
-  return ( // <div>자식 요소</div> -> children
+  return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식 요소</div>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => {
+          setLight(light === "ON" ? "OFF" : "ON")
+        }}>전구 끄기/켜기</button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => {
+          setCount(count + 1)
+        }}>
+          +
+        </button>
+      </div>
     </>
   );
 };
