377bb0ccf9b093b2d02459f55c88c7e7d72e55f6
nz663633 <hyunjisim3@gmail.com>
2026-02-10 16:21:02 +0900

    docs: study event handling in React

--- section02/src/components/Button.jsx
+++ section02/src/components/Button.jsx
@@ -1,6 +1,16 @@
 const Button = ({ children, text, color = "black" }) => { // props 구조 분해 할당
+    // 이벤트 객체
+    const onClickButton = (e) => {
+        console.log(e);
+        console.log(text);
+    };
+
     return (
-        <button style={{ color: color }}>
+        <button
+            onClick={onClickButton} // 함수 이름만 전달
+            // onMouseEnter={onClickButton}
+            style={{ color: color }}
+        >
             {text} - {color.toUpperCase()}
             {children}
         </button>
