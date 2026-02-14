c249d57349ec06f89302bb7fc38bbb4eaa7e1b06
nz663633 <hyunjisim3@gmail.com>
2026-02-14 21:22:04 +0900

    feat: add counter functionality using state lifting

--- section03/src/components/Controller.jsx
+++ section03/src/components/Controller.jsx
@@ -1,12 +1,25 @@
-const Controller = () => {
+const Controller = ({ onClickButton }) => {
     return (
+        // 원하는 값의 인수를 넘겨주기 위해 각 버튼에 화살표 함수 사용
         <div>
-            <button>-1</button>
-            <button>-10</button>
-            <button>-100</button>
-            <button>+100</button>
-            <button>+10</button>
-            <button>+1</button>
+            <button onClick={() => {
+                onClickButton(-1)
+            }}>-1</button>
+            <button onClick={() => {
+                onClickButton(-10)
+            }}>-10</button>
+            <button onClick={() => {
+                onClickButton(-100)
+            }}>-100</button>
+            <button onClick={() => {
+                onClickButton(100)
+            }}>+100</button>
+            <button onClick={() => {
+                onClickButton(10)
+            }}>+10</button>
+            <button onClick={() => {
+                onClickButton(1)
+            }}>+1</button>
         </div>
     )
 };
