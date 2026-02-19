7dcbcc7ca2f781134ffc304b223cc1dcff0a8567
nz663633 <hyunjisim3@gmail.com>
2026-02-19 23:56:54 +0900

    feat: implement todo creation

--- section05/src/components/Editor.jsx
+++ section05/src/components/Editor.jsx
@@ -1,11 +1,40 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    }
+
+    const onKeydown = (e) => { // enter키를 누르면 onSubmit을 실행하도록 처리
+        if (e.key === "Enter") {
+            onSubmit();
+        }
+    }
+
+    const onSubmit = () => {
+        if (content === "") { // 입력값이 비어 있으면?
+            contentRef.current.focus(); // 입력창에 다시 포커스 추가
+            return;
+        }
+        onCreate(content);
+        setContent(""); // 새로운 Todo 추가하고 나서 입력창 초기화
+    }
     return (
-    <div className="Editor">
-        <input placeholder="새로운 Todo..." />
-        <button>추가</button>
-    </div>
+        <div className="Editor">
+            <input
+                ref={contentRef}
+                value={content}
+                onKeyDown={onKeydown}
+                onChange={onChangeContent}
+                placeholder="새로운 Todo..."
+            />
+            <button onClick={onSubmit}>추가</button>
+        </div>
     )
 }
 
