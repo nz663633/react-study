8b8c49fa2f49866945b0b1391c9b209a931927e7
nz663633 <hyunjisim3@gmail.com>
2026-02-21 20:55:29 +0900

    docs: refactor todo list using useReducer

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
-import Exam from './components/Exam';
 
 const mockData = [
   {
@@ -26,52 +25,65 @@ const mockData = [
   }
 ];
 
+function reducer(state, action) { 
+  // state: 현재 todos 배열
+  // action: 어떻게 바꾸고 싶은지에 대한 설명
+  // 새로운 배열 생성 후 반드시 return 할 것
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state]
+    case 'UPDATE':
+      return state.map((item) =>
+        item.id === action.targetId
+          ? { ...item, isDone: !item.isDone } // 체크박스만 토글
+          : item // 나머지 요소는 그대로
+      );
+    case 'DELETE':
+      return state.filter((item) =>
+        item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3); // 컴포넌트가 리렌더링되어도 값이 유지됨(다음 todo의 id 값을 저장하기 위해 사용)
 
-  const onCreate = (content) => { // 새로운 todoItem을 객체형태로 생성
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime()
-    }
-
-    setTodos([newTodo, ...todos]) // React는 상태를 불변하게 관리해야 함
-    // 기존 배열을 직접 수정하는 push 대신 새로운 배열을 만들어서 spread 문법 사용
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime()
+      }
+    });
   };
 
   const onUpdate = (targetId) => {
-    // todo State의 값들 중에
-    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone을 변경
-
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 바꾼 새로운 배열
-    setTodos(todos.map((todo) => { // map은 항상 새로운 배열 반환
-      if (todo.id === targetId) {
-        return {
-          ...todo, // 기존의 todo 값을 펼쳐주고 
-          isDone: !todo.isDone
-        }
-      }
-      return todo
-    }))
-  }
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId
+    });
+  };
 
   const onDelete = (targetId) => {
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(todos.filter((todo) => todo.id !== targetId));
-  }; // filter는 조건이 true인 것만 모아서 새로운 배열을 생성
+    dispatch({
+      type: "DELETE",
+      targetId: targetId
+    });
+  };
 
   return (
     <div className='App'>
-      <Exam />
-      {/* <Header />
+      <Header />
       <Editor onCreate={onCreate} />
       <List
         todos={todos}
         onUpdate={onUpdate}
-        onDelete={onDelete} /> */}
+        onDelete={onDelete} />
     </div>
   )
 }
