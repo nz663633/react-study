4a1fdce34837ab16d2980567968a4e418fe952c4
nz663633 <hyunjisim3@gmail.com>
2026-03-24 21:47:07 +0900

    feat: implement UI for Home page

--- section06/src/pages/Home.jsx
+++ section06/src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import { useSearchParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
 
 const Home = () => {
-    const [params, setParams] = useSearchParams();
-    console.log(params.get("value"));
-    return <div>Home</div>;
+    return (
+        <div>
+            <Header title={"2026년 3월"}
+                leftChild={<Button text={"<"} />}
+                rightChild={<Button text={">"} />}
+            />
+            <DiaryList />
+        </div>
+    );
 };
 
 export default Home;
