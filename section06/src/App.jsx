88a2a11bd9ca22360be9e8d382791b2b54ff5726
nz663633 <hyunjisim3@gmail.com>
2026-05-25 10:53:31 +0900

    feat: add localStorage and fix errors

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useReducer, useRef, createContext } from 'react'
+import { useReducer, useRef, createContext, useEffect, useState } from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Diary from "./pages/Diary"
@@ -7,41 +7,36 @@ import New from "./pages/New"
 import Notfound from "./pages/Notfound"
 import Edit from './pages/Edit'
 
-const mockData = [
-  {
-    id: 1,
-    createdDate: new Date("2026-05-06").getTime(),
-    emotionId: 1,
-    content: "1번 일기 내용"
-  },
-  {
-    id: 2,
-    createdDate: new Date("2026-05-05").getTime(),
-    emotionId: 2,
-    content: "2번 일기 내용"
-  },
-  {
-    id: 3,
-    createdDate: new Date("2026-04-01").getTime(),
-    emotionId: 3,
-    content: "3번 일기 내용"
-  }
-]
-
+// reducer 함수가 실행 시, 데이터를 CREATE, UPDATE, DELETE 할 때마다
+// nextState라는 변수에 값을 담아서 localStorage에 'diary'라는 key 값으로
+// 일기의 현재 데이터를 localStorage에 저장(일종의 데이터베이스 역할)
 function reducer(state, action) {
+  let nextState;
+
   switch (action.type) {
-    case 'CREATE':
-      return [action.data, ...state];
-    case 'UPDATE':
-      return state.map((item) =>
+    case 'INIT':
+      return action.data;
+    case 'CREATE': {
+      nextState = [action.data, ...state];
+      break;
+    }
+    case 'UPDATE': {
+      nextState = state.map((item) =>
         String(item.id) == String(action.data.id) ? action.data : item
       );
-    case 'DELETE':
-      return state.filter((item) =>
+      break;
+    }
+    case 'DELETE': {
+      nextState = state.filter((item) =>
         String(item.id) !== String(action.id)); // action 객체와 같은 id를 갖는 객체를 제거
+      break;
+    };
     default:
       return state;
   }
+
+  localStorage.setItem('diary', JSON.stringify(nextState));
+  return nextState;
 }
 
 // 모든 페이지에서 data값, onCreate, onUpdate, onDelete를 공급받게 함
@@ -52,8 +47,47 @@ export const DiaryDispatchContext = createContext();
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef();
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('diary');
+    if (!storedData) { return; }
+
+    const parsedData = JSON.parse(storedData);
+    if (!Array.isArray(parsedData)) {
+      setIsLoading(false);
+      return;
+    }
+
+    let maxId = 0;
+    parsedData.forEach((item) => {
+      if (Number(item.id) > maxId) {
+        maxId = Number(item.id)
+      }
+    });
+
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type: "INIT",
+      data: parsedData
+    });
+    setIsLoading(false);
+  }, []);
+
+  // key값으로는 숫자, 문자열만 가능
+  // 사이트별로 데이터 보관
+  // 직접 삭제하기 전까지 영구적으로 보관
+  // localStorage.setItem('test', 'hello');
+  // localStorage.setItem('person', JSON.stringify({ name: "심현지" }));
+
+  // console.log(localStorage.getItem('test'));
+  // console.log(JSON.parse(localStorage.getItem('person')));
+
+  // localStorage.removeItem('test');
+
 
   // 새로운 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
@@ -88,6 +122,10 @@ function App() {
       id
     })
   };
+
+  if (isLoading) {
+    return <div>데이터 로딩중입니다...</div>;
+  }
 
   return (
     <>
